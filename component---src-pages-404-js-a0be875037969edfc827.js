05573255ffa90b5df1f09ff7846da1ea7861d08a
Matthew Feng <mattfeng@mit.edu>
2023-01-22 16:30:46 -0500

    Updates

---
+++ component---src-pages-404-js-a0be875037969edfc827.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfoundry=self.webpackChunkfoundry||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294);t.default=function(){return u.createElement(u.Fragment,null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a0be875037969edfc827.js.map
